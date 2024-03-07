6037f3bd0af3020b7cfd78c2c3685b243e596a89
omkarmutte <omkarmutte123@gmail.com>
2024-03-07 23:54:23 +0530

    Arrays DSA question

---
+++ src/com/elite/arrays/_49_SubarrayProblem.java
@@ -0,0 +1,47 @@
+package com.elite.arrays;
+
+import java.util.Scanner;
+
+/*
+In this problem we have an array, what we need to do is printing all subarray's for an array
+Example :
+array -> abc
+subarray's ->
+        a
+        a b
+        a b c
+
+        b
+        b c
+
+        c
+*/
+public class _49_SubarrayProblem {
+    // printAllSubArraysForAnArray :: This method takes array as an input and print's all sub arrays for that array
+    public static void printAllSubArraysForAnArray(int[] arr) {
+        // logic
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                for (int k = i; k <= j; k++) {
+                    System.out.print(arr[k]+"\t");
+                }
+                System.out.println();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter size of an array : ");
+        int size = Integer.parseInt(scn.nextLine());
+        int[] arr = new int[size];
+        // taking input elements for an array
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter " + i + " element : ");
+            arr[i] = Integer.parseInt(scn.nextLine());
+        }
+
+        // calling printAllSubArraysForAnArray Method
+        printAllSubArraysForAnArray(arr);
+
+    }
+}
